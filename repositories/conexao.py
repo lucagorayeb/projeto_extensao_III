ee7cc05f668a83f8610ac6526cac541c8f2e86e4
Luca Gorayeb <lucagorayeb@gmail.com>
2026-06-08 08:25:57 -0400

    Criando repositorio do produto.

--- repositories/conexao.py
+++ repositories/conexao.py
@@ -1,10 +1,10 @@
-#!/usr/bin/env python 
+#!/usr/bin/env python
 """
 -----------------------------------------------------
 Program    : conexao.py
-Description: Essa é a interface de conexão base 
-             para o banco de dados. Conforme o 
-             conforme a necessidade mais conexões 
+Description: Essa é a interface de conexão base
+             para o banco de dados. Conforme o
+             conforme a necessidade mais conexões
              seram adicionadas.
 Version    : 0.1
 Author     : Luca Gorayeb <lucagorayeb@gmail.com>
@@ -14,22 +14,22 @@ Lincence   : GNU/GPL v3.0
 Use:
 -----------------------------------------------------
 """
-
 from abc import ABC, abstractmethod
 import sqlite3
 
+
 class ConexaoBanco(ABC):
-    
+
     @abstractmethod
     def conectar(self):
         pass
-    
+
 
 class ConexaoSqlite(ConexaoBanco):
-    
-    def __init__(self, nome_banco : str):
+
+    def __init__(self, nome_banco: str):
         self._nome_banco = nome_banco
-    
+
     @property
     def nome_banco(self):
         return self._nome_banco
@@ -39,3 +39,8 @@ class ConexaoSqlite(ConexaoBanco):
             return sqlite3.connect(self.nome_banco)
         except sqlite3.Error as erro:
             raise RuntimeError(f"Erro ao conectar ao banco: {erro}")
+
+    def criar_tabelas(self):
+        with self.connectar() as conexao:
+            with open("sql/schema.sql", "r", encoding="utf-8") as arquivo:
+                conexao.executeescript(arquivo.read())
---
+++ repositories/produto_repository.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+"""
+-----------------------------------------------------
+Program    : produto_repository.py
+Description:
+Version    : 0.1
+Author     : Luca Gorayeb <lucagorayeb@gmail.com>
+Date       : 04/06/2026
+Lincence   : GNU/GPL v3.0
+-----------------------------------------------------
+Use:
+-----------------------------------------------------
+"""
+from projeto_extensao_III.models.produto import Produto
+from conexao import ConexaoSqlite
+
+
+class ProdutoRepository:
+
+    def salvar(self):
+        pass
+
+    def buscar_por_id(self):
+        pass
+
+    def listar(self):
+        pass
+
+    def atualizar(self, produto):
+        pass
+
+    def remover(self, id):
+        pass
