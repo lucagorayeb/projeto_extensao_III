66cb2c236d8f8c0fd8337d916677cfef3fead46f
Luca Gorayeb <lucagorayeb@gmail.com>
2026-06-09 06:12:27 -0400

    Criando branch de repositorios.

--- repositories/conexao.py
+++ repositories/conexao.py
@@ -1,10 +1,10 @@
-#!/usr/bin/env python 
+#!/usr/bin/env python
 """
 -----------------------------------------------------
 Program    : conexao.py
-Description: Essa é a interface de conexão base 
-             para o banco de dados. Conforme o 
-             conforme a necessidade mais conexões 
+Description: Essa é a interface de conexão base
+             para o banco de dados. Conforme o
+             conforme a necessidade mais conexões
              seram adicionadas.
 Version    : 0.1
 Author     : Luca Gorayeb <lucagorayeb@gmail.com>
@@ -14,28 +14,34 @@ Lincence   : GNU/GPL v3.0
 Use:
 -----------------------------------------------------
 """
-
 from abc import ABC, abstractmethod
 import sqlite3
 
+
 class ConexaoBanco(ABC):
-    
+
     @abstractmethod
     def conectar(self):
         pass
-    
+
 
 class ConexaoSqlite(ConexaoBanco):
-    
-    def __init__(self, nome_banco : str):
+
+    def __init__(self, nome_banco: str):
         self._nome_banco = nome_banco
-    
+
     @property
     def nome_banco(self):
         return self._nome_banco
 
-    def conectar(self) -> sqlite3.Connection:
-        try:
-            return sqlite3.connect(self.nome_banco)
-        except sqlite3.Error as erro:
-            raise RuntimeError(f"Erro ao conectar ao banco: {erro}")
+    def conectar(self, sql: str):
+        with sqlite3.connect(self._nome_banco) as conexao:
+            with conexao:
+                with conexao.cursor() as cursor:
+                    cursor.execute(sql)
+                    rows = cursor.fetchall()
+                    return rows
+    #    try:
+    #        return sqlite3.connect(self.nome_banco)
+    #    except sqlite3.Error as erro:
+    #        raise RuntimeError(f"Erro ao conectar ao banco: {erro}")
