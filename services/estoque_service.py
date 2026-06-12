cad61d6b8a8248a4d6f28bc7bbbfd15b95d6745b
Luca Gorayeb <lucagorayeb@gmail.com>
2026-06-12 00:52:53 -0400

    Criando os services.

---
+++ services/estoque_service.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+"""
+-----------------------------------------------------
+Program    : estoque_service.py
+Description:
+Version    : 0.1
+Author     : Luca Gorayeb <lucagorayeb@gmail.com>
+Date       : 12/06/2026
+Lincence   : GNU/GPL v3.0
+-----------------------------------------------------
+Use:
+-----------------------------------------------------
+"""
