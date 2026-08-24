6d2f948701c33548c773d65e7ccff4a38c01246f
Luca Gorayeb <lucagorayeb@gmail.com>
2026-08-24 01:12:48 -0400

    fix: Removing frontend, a new will be created.

--- __init__.py
+++ __init__.py
@@ -1,5 +1,13 @@
-from . import models 
-from . import repositories
-from . import services
-from . import views
-from . import api 
+#!/usr/bin/env python
+"""
+-----------------------------------------------------
+Program    : __init__.py
+Description:
+Version    : 0.1
+Author     : Luca Gorayeb <lucagorayeb@gmail.com>
+Date       : 13/06/2026
+Lincence   : GNU/GPL v3.0
+-----------------------------------------------------
+Use:
+-----------------------------------------------------
+"""
